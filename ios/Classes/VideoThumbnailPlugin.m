4e4f3338371849fb774f8f665146007316c32cfe
Kazuki Kobashi <>
2022-11-09 18:22:44 +0900

    changed file name

--- ios/Classes/VideoThumbnailPlugin.m
+++ ios/Classes/VideoThumbnailPlugin.m
@@ -24,9 +24,9 @@
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    
+
     NSDictionary *_args = call.arguments;
-    
+
     NSString *file = _args[@"video"];
 
     NSMutableDictionary * headers = _args[@"headers"];
@@ -39,37 +39,37 @@
     int quality = [[_args objectForKey:@"quality"] intValue];
     _args = nil;
     bool isLocalFile = [file hasPrefix:@"file://"] || [file hasPrefix:@"/"];
-    
+
     NSURL *url = [file hasPrefix:@"file://"] ? [NSURL fileURLWithPath:[file substringFromIndex:7]] :
       ( [file hasPrefix:@"/"] ? [NSURL fileURLWithPath:file] : [NSURL URLWithString:file] );
-    
+
     if ([@"data" isEqualToString:call.method]) {
 
         dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
             //Background Thread
             result([VideoThumbnailPlugin generateThumbnail:url headers:headers format:format maxHeight:maxh maxWidth:maxw timeMs:timeMs quality:quality]);
         });
-        
+
     } else if ([@"file" isEqualToString:call.method]) {
         if( [path isEqual:[NSNull null]] && !isLocalFile ) {
             path = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) lastObject];
         }
-        
+
         dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(void){
             //Background Thread
-           
+
             NSData *data = [VideoThumbnailPlugin generateThumbnail:url headers:headers format:format maxHeight:maxh maxWidth:maxw timeMs:timeMs quality:quality];
             NSString *ext = ( (format == 0 ) ? @"jpg" : ( format == 1 ) ? @"png" : @"webp" );
             NSURL *thumbnail = [[url URLByDeletingPathExtension] URLByAppendingPathExtension:ext];
 
             if(path && [path isKindOfClass:[NSString class]] && path.length>0) {
-                NSString *lastPart = [thumbnail lastPathComponent];
+                NSString *lastPart = [NSString stringWithFormat:@"%d%@", (int)arc4random_uniform(10000), [thumbnail lastPathComponent]];
                 thumbnail = [NSURL fileURLWithPath:path];
                 if( ![[thumbnail pathExtension] isEqualToString:ext] ) {
                     thumbnail = [thumbnail URLByAppendingPathComponent:lastPart];
                 }
             }
-            
+
             NSError *error = nil;
             if( [data writeToURL:thumbnail options:0 error:&error] != YES ) {
                 if( error != nil ) {
@@ -87,35 +87,35 @@
                 }
             }
         });
-        
+
     } else {
         result(FlutterMethodNotImplemented);
     }
 }
 
 + (NSData *)generateThumbnail:(NSURL*)url headers:(NSMutableDictionary*)headers  format:(int)format maxHeight:(int)maxh maxWidth:(int)maxw timeMs:(int)timeMs quality:(int)quality {
-    
+
     AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:url options: [headers isEqual:[NSNull null]] ? nil : @{@"AVURLAssetHTTPHeaderFieldsKey" : headers}];
     AVAssetImageGenerator *imgGenerator = [[AVAssetImageGenerator alloc] initWithAsset:asset];
-    
+
     imgGenerator.appliesPreferredTrackTransform = YES;
     imgGenerator.maximumSize = CGSizeMake((CGFloat)maxw, (CGFloat)maxh);
     imgGenerator.requestedTimeToleranceBefore = kCMTimeZero;
     imgGenerator.requestedTimeToleranceAfter = CMTimeMake(100, 1000);
-    
+
     NSError *error = nil;
     CGImageRef cgImage = [imgGenerator copyCGImageAtTime:CMTimeMake(timeMs, 1000) actualTime:nil error:&error];
-    
+
     if( error != nil ) {
         NSLog(@"couldn't generate thumbnail, error:%@", error);
         return nil;
     }
-    
+
     if( format <= 1 ) {
         UIImage *thumbnail = [UIImage imageWithCGImage:cgImage];
-        
+
         CGImageRelease(cgImage);  // CGImageRef won't be released by ARC
-        
+
         if( format == 0 ) {
             CGFloat fQuality = ( CGFloat) ( quality * 0.01 );
             return UIImageJPEGRepresentation( thumbnail, fQuality );
@@ -130,17 +130,17 @@
         }
         CGImageAlphaInfo ainfo = CGImageGetAlphaInfo( cgImage );
         CGBitmapInfo binfo = CGImageGetBitmapInfo( cgImage );
-        
+
         CGDataProviderRef dataProvider = CGImageGetDataProvider(cgImage);
         CFDataRef imageData = CGDataProviderCopyData(dataProvider);
         UInt8 *rawData = ( UInt8 * ) CFDataGetBytePtr(imageData);
-        
+
         int width = ( int ) CGImageGetWidth(cgImage);
         int height = ( int ) CGImageGetHeight(cgImage);
         int stride = ( int ) CGImageGetBytesPerRow(cgImage);
         size_t ret_size = 0;
         uint8_t *output = NULL;
-        
+
         // preprocess the data for libwebp
         if( ainfo == kCGImageAlphaPremultipliedFirst || ainfo == kCGImageAlphaNoneSkipFirst ) {
             if( ( binfo & kCGBitmapByteOrderMask ) == kCGBitmapByteOrder32Little ) {
@@ -149,7 +149,7 @@
                     ret_size = WebPEncodeLosslessBGRA(rawData, width, height, stride, &output);
                 else
                     ret_size = WebPEncodeBGRA(rawData, width, height, stride, (float)quality, &output);
-            } else 
+            } else
                 if( ( binfo & kCGBitmapByteOrderMask ) == kCGBitmapByteOrder32Big ) {
                     // Big-endian ( iPhone Simulator )
                     for(int y = 0;y<height;y++) {
@@ -171,7 +171,7 @@
         CGDataProviderRelease(dataProvider);
         CFRelease(imageData);
         CGColorSpaceRelease(colorSpace);
-        
+
         if (ret_size == 0) {
             return nil;
         }
